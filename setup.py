d3ad8d8866652f4e910e47a8e797c4d81fc4469e
manasauriz <izzymansoor42@gmail.com>
2025-01-05 17:50:10 +0530

    include projects folder while installation

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = 'ascii_animator',
     version = '1.0.0',
     packages = find_packages(),
+    include_package_data=True, 
     entry_points = {
         'console_scripts': [
             'ascii_animator = ascii_animator.main:main',
